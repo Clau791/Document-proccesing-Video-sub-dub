e406145efd40f9d6166ac16ccc359e04e0575344
Clau <klau2015.cc@gmail.com>
2025-10-11 23:41:26 +0300

    added for hosting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Document-proccesing-Video-sub-dub/', // ← numele repo-ului (exact)
-
   server: {
     port: 5173,        // Portul pe care rulează frontend-ul
     open: true,        // Deschide browser-ul automat
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-/// for github pages
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Document-proccesing-Video-sub-dub/', // ← numele repo-ului (exact)
-})
